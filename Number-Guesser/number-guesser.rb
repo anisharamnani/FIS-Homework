0b362de03ca9d53b4e1b667e3ccfed3af2500fec
Flatiron <flatiron@new-host-25.home>
2013-11-14 18:26:27 -0500

    updated number guesser

--- Number-Guesser/number-guesser.rb
+++ Number-Guesser/number-guesser.rb
@@ -1,12 +1,18 @@
 
-num = Random.new.rand(1..100)
-# Should ask the user to enter a number between 1 and 100
-puts "Please choose a number between 1 and 100"
-# Should receive input from the user
-user_num = gets.chomp.to_i
-# Should tell the user if they were correct or incorrect
-if num == user_num 
-	puts "You are correct! You can see into the computer's brain!"
-else 
-	puts "You are incorrect. I am sorry. "
+while true
+	num = Random.new.rand(1..100)
+	# Should ask the user to enter a number between 1 and 100
+	puts "Please choose a number between 1 and 100"
+	# Should receive input from the user
+	user_num = gets.chomp.to_i
+	# Should tell the user if they were correct or incorrect
+
+	if num == user_num 
+		puts "You are correct! You can see into the computer's brain!"
+		break
+	elsif num > user_num 
+		puts "Your guess was too low. Please try again."
+	elsif num < user_num
+		puts "Your number is to high. please try again."
+	end 
 end 
